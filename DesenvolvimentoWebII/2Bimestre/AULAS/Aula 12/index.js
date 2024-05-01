f4e2bf608d2d96abf293aca0e190a6fd4466813a
Vinicius de Paula <viniciuusdepaula@gmail.com>
2024-04-30 22:45:38 -0300

    update DW

--- DesenvolvimentoWebII/2Bimestre/AULAS/Aula 12/index.js
+++ DesenvolvimentoWebII/2Bimestre/AULAS/Aula 12/index.js
@@ -1,17 +1,36 @@
-const {createApp} = Vue
+const ComponenteA = {
+    template: `
+    <div class="componente">
+        <h1>Login</h1>
+        <label>Email: </label> <input type="email">
+        <label>Senha: </label> <input type="password">
+    </div>
+    `
+}
+
+const ComponenteB = {
+    template: `
+    <div class="componente">
+        <h1>Cadastro</h1>
+        <label>Email: </label> <input type="email">
+        <label>Senha: </label> <input type="password">
+        <label>Confirmar Senha: </label> <input type="password">
+    </div>
+    `
+}
+
+const {createApp} = Vue;
 
 createApp({
-    data(){
+    data() {
         return {
             componenteAtual: "ComponenteA"
         }
     },
     methods: {
         alterarComponentes() {
-            this.componenteAtual = (this.componenteAtual === "ComponenteA")
-            ? "ComponenteB" : "ComponenteA"
+            this.componenteAtual = (this.componenteAtual === "ComponenteA") ? "ComponenteB" : "ComponenteA"
         }
-
     },
     components: {
         ComponenteA,
